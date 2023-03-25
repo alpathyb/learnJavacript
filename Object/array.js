31421737565b072582722f0aecbc6bbbef40bd3a
alpathyb <fjralfath11@gmail.com>
2023-03-25 12:23:34 +0700

    array

---
+++ Object/array.js
@@ -0,0 +1,16 @@
+// this file is for learning an array in js
+
+let myArray = ['hello world',11,20,'good morning','end'];
+
+console.log(myArray);
+console.log(myArray[1]);
+
+// now we delete and add items in myArray
+myArray.push('Start','second');
+
+//for deleting the last item in array
+myArray.pop();
+
+//for add item in the firs place
+myArray.unshift('this is first');
+console.log(myArray);

32e1e74f7be83e927b5484553a682bfd51f35420
alpathyb <fjralfath11@gmail.com>
2023-03-27 18:25:04 +0700

    revisi code

--- loop/learnFor.js
+++ loop/learnFor.js
@@ -1,5 +1,6 @@
 let baris = 5;
 let pola = '';
+let pola2 = '';
 
 for(let i = 0; i <= baris; i++){
     for(let n = 0; n <= i; n++){
@@ -9,3 +10,14 @@ for(let i = 0; i <= baris; i++){
 }
 
 console.log(pola);
+
+// now lets do a reverse
+
+for (let a = 0; a > baris; a++){
+    for(let b = 5; b >= a; b--){
+        pola2 += 'x';
+    }
+    pola2 += '\n';
+}
+
+console.log(pola2);

983bc94f19a48dfa606a0a0bb5b742de005f4d77
alpathyb <fjralfath11@gmail.com>
2023-03-30 19:17:08 +0700

    Quiz 2 submit

--- Object/quiz2.js
+++ Object/quiz2.js
@@ -7,3 +7,17 @@ for(let i=1; i<=100; i++){
 }
 
 console.log(evenNumber);
+
+// now lets do a map quiz 
+
+let currency = new Map([
+    ['USD',14000],
+    ['JPY', 131],
+    ['SGD', 11000],
+    ['MYR', 3500],
+]);
+
+const priceInJPY = 5000;
+
+let priceInIDR = (priceInJPY * currency.get('JPY'));
+console.log(priceInIDR);

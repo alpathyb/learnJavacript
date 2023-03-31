427562048b965383ca807055b3284aabe109239d
alpathyb <fjralfath11@gmail.com>
2023-03-31 12:27:56 +0700

    function learning

---
+++ Function/func1.js
@@ -0,0 +1,15 @@
+// create 3 function to multiply, add, and divide
+
+function multiply(a,b){
+    return a*b;
+}
+
+function add(a,b){
+    return a+b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+console.log(add(2,3),multiply(2,3),divide(6,2));

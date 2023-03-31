f9411db59e4b3d2d6c44b23af0476fffba14a0a9
alpathyb <fjralfath11@gmail.com>
2023-03-31 18:13:02 +0700

    add func

--- Function/func1.js
+++ Function/func1.js
@@ -13,3 +13,22 @@ function divide(a,b){
 }
 
 console.log(add(2,3),multiply(2,3),divide(6,2));
+
+// Now lets create the function to check give the simple of multipy, add and divide
+
+
+function test(d,e,f){
+    let hasil;
+    if(f === 'kali'){
+        hasil = d*e;
+    }else if(f === 'bagi'){
+        hasil = d/e;
+    }else if(f === 'tambah'){
+        hasil = d+e;
+    }else{
+        console.log('Undefined');
+    }
+    return hasil;
+}
+
+console.log(test(2,5,'bagi'));

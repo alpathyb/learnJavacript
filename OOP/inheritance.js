fbbefb12008071cefe85355958f1ce349674f9b7
alpathyb <fjralfath11@gmail.com>
2023-04-10 17:51:29 +0700

    add inheritance

--- OOP/inheritance.js
+++ OOP/inheritance.js
@@ -1,6 +1,5 @@
-//Defining the problems based on what we need
-
-class mailService{
+//Defining the problems base
+/*class mailService{
     constructor(sender) {
         this.sender = sender;
     }
@@ -21,3 +20,39 @@ const gmailService = new mailService('alpathyb@campaign.com');
 
 whatsAppServ.sendBroadcast('Hello! Happy Iftar for all of you!', [1,2,3]);
 gmailService.sendMessage('Hello! my name is alfath', 'rani@gmail,com');
+*/
+
+//Now create inheritance verse
+
+class mailService{
+    constructor(sender){
+        this.sender = sender;
+    }
+
+    sendMessage(message, receiver){
+        console.log(`Sent this message: ${message}, to ${receiver}`);
+    }
+}
+
+class waService extends mailService{
+    broadMessage(message, receivers){
+        for(const receiver of receivers){
+            console.log(`Send this message: ${message} to ${receiver}`);
+        }
+    }
+}
+
+class gmailService extends mailService{
+    operationMessage(message, receiver){
+        if(message === "@#$*"){
+            console.log(`The Sender: ${this.sender} send this message ${message} to: ${receiver}`);
+        }
+    }
+}
+
+const userGmail = new gmailService('alpathyb@gmail.com');
+const userWa = new waService('fjralfath11@gmail.com');
+
+userGmail.operationMessage('Hello docs','fjralfath11@gmail.com');
+userWa.sendMessage('Hello friend, this is alfath','fjr');
+userWa.broadMessage('This is broadcast',['raso','mico','dolo','ronald']);

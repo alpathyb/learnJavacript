642986c1c978463ad7410a503c46e46d8fbc666c
alpathyb <fjralfath11@gmail.com>
2023-04-20 14:43:36 +0700

    new origin class : array

--- OOP/naturalClass.js
+++ OOP/naturalClass.js
@@ -21,3 +21,29 @@ const timeCoventry = date.toLocaleDateString('en-GB',{
 console.log(timeAceh);
 console.log(timeJakarta);
 console.log(timeCoventry);
+
+console.log('===================================');
+
+//let's create another original class
+
+class UniqueArray extends Array {
+    constructor(...args){
+        const uniqueValue = args.filter((item, index)=> args.indexOf(item) === index);
+        super(...uniqueValue);
+    }
+
+    push(item){
+        if (!this.includes(item)){
+            super.push(item);
+        }
+    }
+
+}
+
+const numberArray = new UniqueArray(1,2,3,1,2,3);
+console.log(numberArray);
+
+// push some items with same number to the array
+numberArray.push(1);
+numberArray.push(4);
+console.log(numberArray);

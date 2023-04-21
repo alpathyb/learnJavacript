0393f1c97fa3a06220d4bb2ac123a49d17eefd9d
alpathyb <fjralfath11@gmail.com>
2023-04-21 20:33:21 +0700

    new revision of fibonacci sequence

--- functional programming/pureFunction.js
+++ functional programming/pureFunction.js
@@ -1,15 +1,37 @@
 // create two pure function 
 
-const countFibonacci = (a) =>{
-    n1 = 0;
-    n2 = 1;
-    temp = 0;
-    for(let i = 0; i<= a; i++){
-        console.log(n1);
-        temp = n1 + n2;
-        n1 = n2;
-        n2 = temp;
+// count fibonacci to insert iterate value base on a value
+// this is the lates version buat it will return undefined first 
+// const countFibonacci = (a) =>{
+//     n1 = 0;
+//     n2 = 1;
+//     temp = 0;
+//     for(let i = 0; i<= a; i++){
+//         console.log(n1);
+//         temp = n1 + n2;
+//         n1 = n2;
+//         n2 = temp;
+//     }
+// }
+
+// console.log(countFibonacci(5));
+
+// count fibonacci2 to insert iterate value until value x is achieve
+const countFibo2 = (x) =>{
+    let x1 = 0;
+    let x2 = 1;
+    let temp2;
+
+    let result = [];
+
+    while (x1 <= x) {
+        result.push(x1);
+        temp2 = x1 + x2;
+        x1 = x2;
+        x2 = temp2;
+        
     }
+    return result;
 }
 
-console.log(countFibonacci(10));
+console.log(...countFibo2(5));

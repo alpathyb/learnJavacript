572baf82a5d301bdd50880b04b92cdd8fbce7237
alpathyb <fjralfath11@gmail.com>
2023-04-25 15:57:50 +0700

    add my first project

---
+++ Project_1/index.js
@@ -0,0 +1,35 @@
+class coffeMec{    
+    constructor(number){
+        this.number = number;
+    }
+
+    coffeType = [
+        {
+            coffe : 'sanger',
+            condition : 'hot',
+            value : 10000,
+        },
+        {
+            coffe : 'sanger',
+            condition : 'cold',
+            value : 11000,
+        },
+        {
+            coffe : 'robusta',
+            condition : 'hot',
+            value : 12000,
+        },{
+            coffe : 'arabica',
+            condition : 'hot',
+            value : 15000,
+        },
+        
+    ]
+
+    order = (type, quantity) =>{
+        console.log(`You ordered ${quantity} of ${this.coffeType[type].coffe} and the total is Rp ${this.coffeType[type].value * quantity}`);
+    }
+}
+
+const firstOrder = new coffeMec(2);
+firstOrder.order(1, 2);
